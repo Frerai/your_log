7a3e27240dd4e15eeb44310b822310d0c4b0ce16
Feraidoon Hamidi <Feraidoon321@hotmail.com>
2021-10-04 16:58:53 +0200

    Set DEBUG based on environment variables to protect from attackers.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os  # Used to not show detailed errors on webpage
 import django_heroku
 from pathlib import Path
 
@@ -138,3 +139,8 @@ LOGIN_URL = 'users:login'
 
 # Heroku settings for deplying web app.
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG == True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG == False
