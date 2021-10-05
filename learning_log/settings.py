d8feb9407efd2e946c47368349c81564cacda5e3
Feraidoon Hamidi <Feraidoon321@hotmail.com>
2021-10-05 13:15:54 +0200

    Fixed some errors.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os  # Used to not show detailed errors on webpage
 import django_heroku
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5822df@0x3#f$0l^bg^tsspxg&!jn3540+qk2wgv)mwdnr@!v#'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  # Set to True, if wanting to test local
